873df67bf59a865e177460c6ace51a77ebd6c3d3
Annie Bonav <anniebonav@outlook.com>
2023-05-12 13:40:41 +0200

    Adds UI stuff

---
+++ Assets/ChangeCalendarSpeed.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ChangeCalendarSpeed : MonoBehaviour
+{
+    public void OnPoint(InputValue value)
+    {
+        print("Pointed");
+    }
+}
